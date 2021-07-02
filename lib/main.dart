36c3bb4492c64d6f3d3e32337b98d9a2751b2e90
Angel <darkangel0938@gmail.com>
2021-07-02 20:26:14 +0800

    Apply icons for phone and email

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,58 @@ class MyApp extends StatelessWidget {
               Text(
                 'PHP Developer'.toUpperCase(),
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
+                    fontSize: 18,
+                    color: Colors.white70,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                      size: 18,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '8844722',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 18,
+                          letterSpacing: 1,
+                          fontFamily: 'SansPro'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                      size: 18,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'angelica_rys@yahoo.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 18,
+                          fontFamily: 'SansPro'),
+                    ),
+                  ],
                 ),
               ),
             ],
